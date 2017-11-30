34774ea250352ffbcb680a8eca4cee122118650a
Deyvid Yury <deyvidyury@yahoo.com.br>
2017-11-29 22:23:14 -0300

    App Completed

---
+++ src/app/firebase.credentions.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB3Z-JaEYtkSDcqpIOVm5vzPMpQVi9pDPE",
+  authDomain: "shoppinglist-5c3af.firebaseapp.com",
+  databaseURL: "https://shoppinglist-5c3af.firebaseio.com",
+  projectId: "shoppinglist-5c3af",
+  storageBucket: "",
+  messagingSenderId: "124916100447"
+};
